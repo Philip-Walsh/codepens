0756aab662fd396fa7021f7757615983415eb7ad
Philip-Walsh <philipwalshpw@gmail.com>
2025-02-16 12:41:41 +0000

    add nav practice

---
+++ other/ngo-nav/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+    const toggleNav = document.getElementById("toggle-nav");
+    const navItems = document.querySelector("nav ul");
+  
+    toggleNav.addEventListener("click", () => {
+      navItems.classList.toggle("show");
+    });
+    navItems.addEventListener("click", () => {
+      navItems.classList.remove("show");
+    });
+    setTimeout(() => {
+      navItems.classList.toggle("show");
+    }, 500);
+  
+    setTimeout(() => {
+      navItems.classList.toggle("show");
+    }, 2700);
+  });
+  
