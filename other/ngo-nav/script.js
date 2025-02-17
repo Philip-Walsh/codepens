d39c6470eb7fa1128b46199b068bc109c33fc25a
Philip-Walsh <philipwalshpw@gmail.com>
2025-02-17 09:27:38 +0000

    updated color scheme nav

--- other/ngo-nav/script.js
+++ other/ngo-nav/script.js
@@ -1,19 +1,18 @@
-window.addEventListener("load", () => {
-    const toggleNav = document.getElementById("toggle-nav");
-    const navItems = document.querySelector("nav ul");
-  
-    toggleNav.addEventListener("click", () => {
-      navItems.classList.toggle("show");
-    });
-    navItems.addEventListener("click", () => {
-      navItems.classList.remove("show");
-    });
-    setTimeout(() => {
-      navItems.classList.toggle("show");
-    }, 500);
-  
-    setTimeout(() => {
-      navItems.classList.toggle("show");
-    }, 2700);
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleNav = document.getElementById("toggle-nav");
+  const navItems = document.querySelector("nav ul");
+
+  toggleNav.addEventListener("click", () => {
+    navItems.classList.toggle("show");
   });
-  
+  navItems.addEventListener("click", () => {
+    navItems.classList.remove("show");
+  });
+  setTimeout(() => {
+    navItems.classList.toggle("show");
+  }, 500);
+
+  setTimeout(() => {
+    navItems.classList.toggle("show");
+  }, 2700);
+});
