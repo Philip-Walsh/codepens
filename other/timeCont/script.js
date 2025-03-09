a1fcb1bb3b5c230a0d21423726f418cdc4d223cf
Philip-Walsh <philipwalshpw@gmail.com>
2025-03-09 14:55:22 +0000

    fix time data

--- other/timeCont/script.js
+++ other/timeCont/script.js
@@ -1,57 +1,24 @@
 BASE_CLASSNAME = "time__container";
 let times = {
   exampleid: {
-    solarNoon: "2025-03-07T20:05:22.154Z",
-    nadir: "2025-03-07T08:05:22.154Z",
-    sunrise: "2025-03-07T14:15:10.554Z",
-    sunset: "2025-03-08T01:55:33.755Z",
-    sunriseEnd: "2025-03-07T14:17:45.738Z",
-    sunsetStart: "2025-03-08T01:52:58.571Z",
-    dawn: "2025-03-07T13:50:10.193Z",
-    dusk: "2025-03-08T02:20:34.115Z",
-    nauticalDawn: "2025-03-07T13:21:11.805Z",
-    nauticalDusk: "2025-03-08T02:49:32.503Z",
-    nightEnd: "2025-03-07T12:52:09.117Z",
-    night: "2025-03-08T03:18:35.191Z",
-    goldenHourEnd: "2025-03-07T14:48:29.625Z",
-    goldenHour: "2025-03-08T01:22:14.683Z",
+    solarNoon: new Date("2025-03-07T20:05:22.154Z"),
+    nadir: new Date("2025-03-07T08:05:22.154Z"),
+    sunrise: new Date("2025-03-07T14:15:10.554Z"),
+    sunset: new Date("2025-03-08T01:55:33.755Z"),
+    sunriseEnd: new Date("2025-03-07T14:17:45.738Z"),
+    sunsetStart: new Date("2025-03-08T01:52:58.571Z"),
+    dawn: new Date("2025-03-07T13:50:10.193Z"),
+    dusk: new Date("2025-03-08T02:20:34.115Z"),
+    nauticalDawn: new Date("2025-03-07T13:21:11.805Z"),
+    nauticalDusk: new Date("2025-03-08T02:49:32.503Z"),
+    nightEnd: new Date("2025-03-07T12:52:09.117Z"),
+    night: new Date("2025-03-08T03:18:35.191Z"),
+    goldenHourEnd: new Date("2025-03-07T14:48:29.625Z"),
+    goldenHour: new Date("2025-03-08T01:22:14.683Z"),
   },
 };
 times = {};
-const getTimeInTimezone1 = (timezone) => {
-  let timeInTimezone =new Date(new Date().toLocaleString("en-US", { timeZone: timezone }));
-  // console.log({timezone},{timeInTimezone});
-  return timeInTimezone;
-};
-const getTimeInTimezone = (timezone) => {
-  // let timeInTimezone =new Date(new Date().toLocaleString("en-US", { timeZone: timezone }));
-  let options = {
-    timeZone: timezone,
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  },
-  formatter = new Intl.DateTimeFormat([], options);
-  let timeInTimezone = formatter.format(new Date());
-  // console.log({timezone},{timeInTimezone});
-  return timeInTimezone;
-};
 
-function getTime(timezone) {
-  let time1 = getTimeInTimezone1(timezone);
-  console.log({time1});
-  let time = getTimeInTimezone(timezone);
-  console.log({time});
-  return [
-    time,
-    time.toLocaleTimeString("en-US", {
-      timeZone: timezone,
-    }),
-  ];
-}
 function getTimeComponents(id) {
   let timeParent = document.getElementById(id);
   let timeElement = document.querySelector(`#${id} time`);
@@ -67,7 +34,7 @@ function getTimeComponents(id) {
 }
 
 function getTimeClass(time, timeRanges) {
-  console.log({ time });
+  console.log('getTimeClass', time);
   const ranges = [
     {
       range: [timeRanges.sunrise, timeRanges.sunriseEnd],
@@ -113,7 +80,7 @@ function getTimeClass(time, timeRanges) {
   ];
   for (const { range, className } of ranges) {
     if (time >= range[0] && time < range[1]) {
-      console.log(className);
+      console.log(`Picked ${className} as the time: {${time}} is between [${range[0]}] and [${range[1]}]`);
       return `time__${className}`;
     }
   }
@@ -128,18 +95,31 @@ function updateClassesIfChanged(timeParent, newClassName) {
     console.log("updated");
   }
 }
+const getTime = (timezone) => {
+  let now = new Date();
+  let formattedTime = now.toLocaleTimeString("en-US", { timeZone: timezone });
+  let timeObj = new Date(now.toLocaleString("en-US", { timeZone: timezone }));
+  return [timeObj, formattedTime];
+};
 
 const updateTime = (id, timezone, lat, lon) => {
   console.log(id);
   let [timeParent, timeElement] = getTimeComponents(id);
   let [time, formattedTime] = getTime(timezone);
-  console.log(time);
+  // console.log(time, formattedTime);
   timeElement.textContent = formattedTime;
 
   if (!times[id]) {
-    times[id] = SunCalc.getTimes(time, lat, lon);
-    console.log(times[id]);
-  }
+
+    let utcTimes = SunCalc.getTimes(time, lat, lon);
+    times[id] = Object.fromEntries(
+      Object.entries(utcTimes).map(([key, value]) => [
+        key,
+        new Date(value.toLocaleString("en-US", { timeZone: timezone })),
+      ])
+    );
+}
+
   let newClassName = getTimeClass(time, times[id]);
 
   updateClassesIfChanged(timeParent, newClassName);
@@ -148,7 +128,8 @@ const updateTime = (id, timezone, lat, lon) => {
 const addTicker = (id, timezone, lat, lon) => {
   updateTime(id, timezone, lat, lon);
   // setInterval(() => updateTime(id, timezone, lat, lon), 1000);
+
 };
 
 addTicker("indiana", "America/Kentucky/Louisville", 39.7684, -86.1581);
-// addTicker("dublin", "Europe/Dublin", 53.3498, -6.2603);
+addTicker("dublin", "Europe/Dublin", 53.3498, -6.2603);
